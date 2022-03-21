32a668938410f5b08104853a5719ef1cedf75ef2
Yana-Hangabina <yan@hongb.in>
2022-03-21 19:17:15 +0800

    feat: fix hd logo and add nodemon

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -4,7 +4,7 @@ import Logo from '../../assets/logo.png'
 export const Home = () => {
   return (
     <div>
-      <img src={Logo} alt="logo" />
+      <img src={Logo} alt="logo" style={{width: 256, height: "auto"}}/>
       <p>resume</p>
     </div>
   )
