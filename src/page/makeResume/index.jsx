4d3cdcf5bb33d7ca170804a407e382362c8d1211
jjlstruggle <2020213994@stu.cqupt.edu.cn>
2022-03-22 10:38:02 +0800

    feat: test Drag

--- src/page/makeResume/index.jsx
+++ src/page/makeResume/index.jsx
@@ -3,22 +3,37 @@ import React from "react";
 import { TopHeader } from "../../components/topHeader";
 import styled from "styled-components";
 import { useNavigate } from "react-router";
+import { useDrag } from "react-dnd";
 export const MakeResume = () => {
   const navigate = useNavigate();
+  const [collect, dragDiv] = useDrag(() => {
+    return {
+      type: "",
+    };
+  }, []);
   return (
     <div>
       <TopHeader
         render={() => {
           return (
             <BtnGroup>
-              <Button type={'primary'} size={'large'} >新建简历</Button>
-              <Button type={'primary'} size={'large'} onClick={() => {
-                navigate('/resumetemp')
-              }}>使用模板</Button>
+              <Button type={"primary"} size={"large"}>
+                新建简历
+              </Button>
+              <Button
+                type={"primary"}
+                size={"large"}
+                onClick={() => {
+                  navigate("/resumetemp");
+                }}
+              >
+                使用模板
+              </Button>
             </BtnGroup>
           );
         }}
       />
+      <DragDiv ref={dragDiv}></DragDiv>
     </div>
   );
 };
@@ -27,4 +42,10 @@ const BtnGroup = styled.div`
   min-width: 200px;
   display: flex;
   justify-content: space-between;
-`
+`;
+
+const DragDiv = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: #1890ff;
+`;
