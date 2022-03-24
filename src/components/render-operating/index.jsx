96af125b69cf2d533695748295834a85e7e7f19a
Yana-Hangabina <yan@hongb.in>
2022-03-24 20:51:52 +0800

    fix: fix operator

--- src/components/render-operating/index.jsx
+++ src/components/render-operating/index.jsx
@@ -1,3 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { Input } from "antd";
+import styled from "styled-components";
+import { useSelector,useDispatch } from 'react-redux'
+// pass in the id of a component, get the style settings of it from redux, then render them, so user can change styles
+// the style settings looks like this:
+// settings = {
+//     style1: {color1: "#000", color2: "#222"},
+//     style2: {color1: "#000", color2: "#222"},
+// }
+export const RenderOperating = (id) => { // 这里传进来的是id
+    
+    const selector = useSelector(state=>state.selector); //状态
+    const dispatch = useDispatch(); //改变状态的方法
+    // const [styles, setStyles] = useState({/*get style settings from redux*/});
+    // useEffect(() => {
+        // change the style settings of the component in the redux
+    // }, [styles]);
+    
+    return (
+        <OperateArea>{() => {
+            for (let styleGroup in selector.currentSettings.keys()) {
+                return (
+                    <StyleGroup>
+                        <StyleGroupTitle>{styleGroup}</StyleGroupTitle>
+                        {(styleGroup) => {
+                            for (let styleItem in styles[styleGroup].keys()) {
+                                return (
+                                    <StyleItem>
+                                        <StyleItemTitle>{styleItem}</StyleItemTitle>
+                                        <Input value={styles[styleGroup][styleItem]} onBlur={(e) => { // 在这里怎么把获取到的新值传给redux捏尼尼尼你直接写  调用dispatch函数呀
+                                            // setStyles((prev) => { // 未验证格式
+                                            //     let newStyles = JSON.parse(JSON.stringify(prev));
+                                            //     newStyles[styleGroup][styleItem] = e.target.value;
+                                            //     return newStyles;
+                                            // })
+                                            dispatch(updateSelectComponent({
+                                                ...selector,
+                                                currentSettings: { /**新值 */},
+                                            }))
+                                        }}/>
+                                    </StyleItem>
+                                );
+                            }
+                        }}
+                    </StyleGroup>
+                )
+            }
+        }}</OperateArea>
+
+};
+
+const OperateArea = styled.div``;
+
+
+
+
+
+
+
 // import React from "react";
 
 // export const RenderOperating = ({ obj }) => {
