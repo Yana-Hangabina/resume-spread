8c5759b66ca3a3fb2dc98df7fa0fc3e66a3bf658
“Providence-byte” <yyyjims842@gmail.com>
2022-03-25 21:22:10 +0800

    feat: fix key bug

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -137,7 +137,7 @@ function Skills({ isShot, cid, $tree }) {
   return settings.map((Item) => {
     const { Component } = SwitchComponent(Item.name);
     return (
-      <InfoContainer>
+      <InfoContainer key={nanoid()}>
         <ComponentTitle title={"个人技能"} />
         <MainContainer>
           <Component
@@ -160,115 +160,6 @@ export default connect((state) => {
   };
 })(Skills);
 
-
-// import React, { useEffect, useState } from "react";
-// import styled from "styled-components";
-// import { ComponentTitle } from "../component-title";
-// import { v4 as uuid } from "uuid";
-
-// const PLACEHOLDER = "人在江湖，须有一技傍身";
-
-// const RenderSkills = (props) => {
-//   const [skillsList, setSkillsList] = useState([]);
-
-//   useEffect(() => {
-//     if (props) {
-//       props("skills", skillsList);
-//     }
-//   // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [skillsList]);
-
-//   if (!props) {
-//     // 快照
-//     return (
-//       <SkillList>
-//         <List>
-//           <Input value={PLACEHOLDER} onChange={() => {}} />
-//         </List>
-//       </SkillList>
-//     );
-//   }
-
-//   return (
-//     <SkillList>
-//       {skillsList.map((item, index) => {
-//         if (item.isEditing === false) {
-//           return (
-//             <List
-//               key={item.id}
-//               onClick={() => {
-//                 setSkillsList((prev) => {
-//                   let newList = [...prev];
-//                   newList[index].isEditing = true;
-//                   return newList;
-//                 });
-//               }}
-//             >
-//               {item.text}
-//             </List>
-//           );
-//         } else {
-//           return (
-//             <List key={item.id}>
-//               <Input
-//                 autoFocus
-//                 onBlur={() => {
-//                   // 已知问题：点击另一项，会触发blur事件，但是另一项不会被选中
-//                   setSkillsList((prev) => {
-//                     let newList = [...prev];
-//                     newList[index].isEditing = false;
-//                     return newList;
-//                   });
-//                 }}
-//                 value={item.text}
-//                 onKeyDown={(e) => {
-//                   if (e.key === "Enter") {
-//                     e.preventDefault();
-//                     setSkillsList((prev) => {
-//                       let newList = [...prev];
-//                       newList[index].isEditing = false;
-//                       newList.splice(index + 1, 0, {
-//                         id: uuid(),
-//                         text: "",
-//                         isEditing: true,
-//                       });
-//                       return newList;
-//                     });
-//                   } else if (e.key === "Backspace" && skillsList.length > 1) {
-//                     if (item.text === "") {
-//                       setSkillsList((prev) => {
-//                         let newList = [...prev];
-//                         newList[index - 1].isEditing = true;
-//                         newList.splice(index, 1);
-//                         return newList;
-//                       });
-//                     }
-//                   }
-//                 }}
-//                 onChange={(e) => {
-//                   setSkillsList((prev) => {
-//                     let newList = [...prev];
-//                     newList[index].text = e.target.value;
-//                     return newList;
-//                   });
-//                 }}
-//               ></Input>
-//             </List>
-//           );
-//         }
-//       })}
-//     </SkillList>
-//   );
-// };
-
-// export const Skills = (props) => {
-//   return (
-//     <InfoContainer>
-//       <ComponentTitle title={"个人技能"} />
-//       <MainContainer>{RenderSkills(props.handleData)}</MainContainer>
-//     </InfoContainer>
-//   );
-// };
 
 const InfoContainer = styled.div``;
 const MainContainer = styled.div`
